2f417de6b36617c494ad2e51d5beb2120484e9b3
Andriy <work@andriy.bokiy.name>
2019-04-10 13:06:08 +0300

    first commit

---
+++ package/system/languages/uk/controllers/comments/widgets/list.php
@@ -0,0 +1,5 @@
+<?php
+
+define('LANG_WD_COMMENTS_LIST_SHOW_TEXT',    'Показувати текст коментарів');
+define('LANG_WD_COMMENTS_LIST_SHOW_AVATARS', 'Показувати аватари авторів');
+define('LANG_WD_COMMENTS_SHOW_LIST', 'Показувати коментарі');
---
+++ package/system/languages/uk/controllers/rating/rating.php
@@ -0,0 +1,18 @@
+<?php
+
+    define('LANG_RATING_CONTROLLER',  'Рейтинг');
+    define('LANG_RATING_DISABLED',    'Ви не можете голосувати');
+    define('LANG_RATING_VOTED',       'Ви вже голосували');
+    define('LANG_RATING_VOTE_UP',     'Сподобалося');
+    define('LANG_RATING_VOTE_DOWN',   'Не сподобалося');
+    define('LANG_RATING_IS_HIDDEN',   'Приховувати підсумковий рейтинг поки не поставлена ​​оцінка');
+    define('LANG_RATING_SHOW_INFO',   'Показувати учасників голосування та їх оцінки');
+    define('LANG_RATING_NO_VOTES',    'Немає оцінок');
+define('LANG_RATING_ALLOW_GUEST_VOTE', 'Дозволити голосування гостям');
+define('LANG_RATING_TEMPLATE', 'Шаблон блоку рейтинга');
+define('LANG_RATING_UP', 'позитивно');
+define('LANG_RATING_DOWN', 'негативно');
+define('LANG_RATING_PM', '%s %s оцінив ваш допис <a href="%s">%s</a>');
+define('LANG_RATING_NOTIFY_VOTE',  'Сповіщати про оцінки дописів');
+define('LANG_RATING_ITEM_LABEL',  'Підпис перед блоком в дописі');
+define('LANG_RATING_LIST_LABEL',  'Підпис перед блоком в списку');
